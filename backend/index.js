35ab89224dd65e1d4bbb9dda7ff433a4666ee32d
bhaveshbalendra <balendrabhavesh@gmail.com>
2024-11-04 12:04:26 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,44 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+ 
+dotenv.config();
 
-dotenv.config({});
-const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+const __dirname = path.resolve();
 
+//middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+    origin: process.env.URL,
+    credentials: true
+}
+app.use(cors(corsOptions));
 
-app.get("/", (_, res) => {
-  res.send("Backend Response");
-});
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server listening at port ${PORT}`);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
+
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
 });
--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -4,8 +4,7 @@ import path from "path";
 const parser = new DataUriParser();
 
 const getDataUri = (file) => {
-  const extName = path.extname(file.originalname).toString();
-  return parser.format(extName, file.buffer).content;
+    const extName = path.extname(file.originalname).toString();
+    return parser.format(extName, file.buffer).content;
 };
-
 export default getDataUri;
